67495b208f21989e5a97fd0dce2395b773c53e18
Marcos Flores <maff.dsgn@gmail.com>
2022-08-17 19:02:46 -0400

    end section one

---
+++ src/app/pages/food-dishes/food-dishes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fooddishes',
+  templateUrl: './food-dishes.component.html',
+  styleUrls: ['./food-dishes.component.scss']
+})
+export class FoodDishesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
