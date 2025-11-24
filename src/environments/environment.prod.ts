e436881b7906ff8fe1febf6444f8fe96183492a8
Marcos Flores <maff.dsgn@gmail.com>
2025-11-24 16:02:16 -0300

    remove folder unused

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBA_9cKTG9i_lB9neftIp0Ns_5YfUvk_nc",
+    authDomain: "mealsapp-6d3f4.firebaseapp.com",
+    projectId: "mealsapp-6d3f4",
+    storageBucket: "mealsapp-6d3f4.firebasestorage.app",
+    messagingSenderId: "599130327839",
+    appId: "1:599130327839:web:ee336b8e0c0311e6f17e2a",
+    measurementId: "G-GCTVZHXRC2"
+  }
 };
